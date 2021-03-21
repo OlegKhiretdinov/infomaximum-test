f8f6227e50b9eb0f08827df99d1bf921d72ebb58
Oleg Khiretdinov <oleg@oleg.win>
2021-03-21 11:59:28 +0300

    refactor: Menu componet use hook

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-
-import { toggleMenu } from '../../redux/menu-reducer';
+import { useState } from 'react';
 
 import cls from './Menu.module.scss';
 import product from '../../assets/product.png';
@@ -10,29 +9,31 @@ import processList from '../../assets/process_list.png';
 
 
 const Menu = (props) => {
+
+  const[menuIsOpen, toggleMenu] = useState(false)
   return (
     <>
       <div className={cls.menu}>
-        <div className={cls.menuTrigger} onClick={props.toggleMenu}>
+        <div className={cls.menuTrigger} onClick={() => toggleMenu(!menuIsOpen)}>
           <div className={`${cls.button} ${cls.icon} ${cls.close}`}></div>
           <div className={cls.title}>Меню</div>
         </div>
       </div>
 
-      <div className={`${cls.overlay} ${props.menuIsOpen ? null : cls.hide}`} onClick={props.toggleMenu}></div>
+      <div className={`${cls.overlay} ${menuIsOpen ? null : cls.hide}`} onClick={() => toggleMenu(!menuIsOpen)}></div>
 
-      <div className={`${cls.list} ${props.menuIsOpen ? cls.open : null}`}>
-        <div className={cls.menuTriggerOpen} onClick={props.toggleMenu}>
+      <div className={`${cls.list} ${menuIsOpen ? cls.open : null}`}>
+        <div className={cls.menuTriggerOpen} onClick={() => toggleMenu(!menuIsOpen)}>
           <div className={`${cls.button} ${cls.icon} ${cls.open}`}></div>
           <div className={cls.title}>
             <img src={product} alt="proceset" />
           </div>
         </div>
-          <NavLink exact activeClassName={cls.active} to="/profile" className={cls.menuLink} onClick={props.toggleMenu}>
+          <NavLink exact activeClassName={cls.active} to="/profile" className={cls.menuLink} onClick={() => toggleMenu(!menuIsOpen)}>
             <img src={user} alt="user"/>
             <span>{props.userName}</span>
           </NavLink>
-          <NavLink exact activeClassName={cls.active} to="/" className={cls.menuLink} onClick={props.toggleMenu}>
+          <NavLink exact activeClassName={cls.active} to="/" className={cls.menuLink} onClick={() => toggleMenu(!menuIsOpen)}>
             <img src={processList} alt="process list"/>
             <span>Список процессов</span>
           </NavLink>
@@ -42,14 +43,7 @@ const Menu = (props) => {
 }
 
 const mapStateToProps = (state) => ({
-  menuIsOpen: state.menu.menuIsOpen,
   userName: state.profile.firstName,
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  toggleMenu: () => {
-    dispatch(toggleMenu())
-  },
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Menu);
+export default connect(mapStateToProps)(Menu);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,16 @@
 import { combineReducers, createStore } from "redux";
 import loginReducer from "./login-reducer";
-import menuReducer from "./menu-reducer";
 import processReducer from "./process-reducer";
 import profileReducer from "./profile-reducer";
 import signUpReducer from "./signUp-reducer";
 
-
 const reducers = combineReducers({
   login: loginReducer,
   signUp: signUpReducer,
-  menu: menuReducer,
   profile: profileReducer,
   processList: processReducer,
 })
 
 const store = createStore(reducers)
-
-window.store = store
 
 export default store;
